79ee2fd793f1c4cd53996644ca79a809bb6b7c7f
Shing Lyu <shing.lyu@gmail.com>
2019-02-18 21:46:39 +0100

    Changed ServiceWorker relateive path for gh-pages

--- sw.js
+++ sw.js
@@ -5,9 +5,9 @@ self.addEventListener('install', function(e) {
   e.waitUntil(
     caches.open('app-cache').then(function(cache) {
       return cache.addAll([
-        '/',
-        '/index.html',
-        '/manifest.json',
+        '/tip-calculator/',
+        '/tip-calculator/index.html',
+        '/tip-calculator/manifest.json',
       ]);
     })
   );
